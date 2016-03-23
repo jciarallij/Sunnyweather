8288a9243b3bde0ee32db4bebadedef0ed9c69a0
jciarallij <jciarallij@gmail.com>
2016-03-22 20:20:22 -0400

    init commit hooked up labels

---
+++ SunnyWeather/SunnyWeather/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  SunnyWeather
+//
+//  Created by Joshua Ciaralli on 3/22/16.
+//  Copyright © 2016 DigitalCrafts. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var dayNameLabel: UILabel!
+    
+    @IBOutlet var todayTempLabel: UILabel!
+    
+    @IBOutlet var todayLowLabel: UILabel!
+    
+    @IBOutlet var todayDescription: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+}
